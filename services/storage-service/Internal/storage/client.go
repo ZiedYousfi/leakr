04f70e8e6350ea954ea7771bafcebf8a14c5f249
ZiedYousfi <yousfi_zied@yahoo.com>
2025-05-07 16:15:55 +0200

    refactor: improve NewClient function for Cloudflare R2 by simplifying endpoint resolution and configuration loading

--- services/storage-service/Internal/storage/client.go
+++ services/storage-service/Internal/storage/client.go
@@ -1,44 +1,47 @@
 package storage
 
 import (
-	"context"
-	"fmt"
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/s3"
+    "context"
+    "fmt"
+
+    "github.com/aws/aws-sdk-go-v2/aws"
+    "github.com/aws/aws-sdk-go-v2/config"
+    "github.com/aws/aws-sdk-go-v2/credentials"
+    "github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
 // Client encapsule un client S3 configuré pour Cloudflare R2
 type Client struct {
-	S3     *s3.Client
-	BucketMain   string
-	BucketBackup string
+    S3           *s3.Client
+    BucketMain   string
+    BucketBackup string
 }
 
 // NewClient initialise un client AWS S3 connecté à R2
-func NewClient(ctx context.Context, accountID, accessKey, secretKey, mainBucket, backupBucket string) (*Client, error) {
-	resolver := aws.EndpointResolverWithOptionsFunc(func(service, region string, options ...interface{}) (aws.Endpoint, error) {
-		if service == s3.ServiceID {
-			endpoint := fmt.Sprintf("https://%s.r2.cloudflarestorage.com", accountID)
-			return aws.Endpoint{URL: endpoint, SigningRegion: "auto"}, nil
-		}
-		return aws.Endpoint{}, fmt.Errorf("unknown endpoint requested")
-	})
+func NewClient(
+    ctx context.Context,
+    accountID, accessKey, secretKey, mainBucket, backupBucket string,
+) (*Client, error) {
+    // Chargement de la configuration avec identifiants statiques
+    cfg, err := config.LoadDefaultConfig(ctx,
+        config.WithCredentialsProvider(
+            credentials.NewStaticCredentialsProvider(accessKey, secretKey, ""),
+        ),
+        config.WithRegion("auto"), // laisse la SDK gérer la signature
+    )
+    if err != nil {
+        return nil, err
+    }
 
-	cfg, err := config.LoadDefaultConfig(ctx,
-		config.WithCredentialsProvider(
-			aws.CredentialsProviderFunc(func(ctx context.Context) (aws.Credentials, error) {
-				return aws.Credentials{
-					AccessKeyID: accessKey,
-					SecretAccessKey: secretKey,
-				}, nil
-			})),
-		config.WithEndpointResolverWithOptions(resolver),
-	)
-	if err != nil {
-		return nil, err
-	}
+    // Construction du client S3 en spécifiant l’endpoint R2
+    endpoint := fmt.Sprintf("https://%s.r2.cloudflarestorage.com", accountID)
+    s3Client := s3.NewFromConfig(cfg, func(o *s3.Options) {
+        o.BaseEndpoint = aws.String(endpoint)
+    })
 
-	s3Client := s3.NewFromConfig(cfg)
-	return &Client{S3: s3Client, BucketMain: mainBucket, BucketBackup: backupBucket}, nil
+    return &Client{
+        S3:           s3Client,
+        BucketMain:   mainBucket,
+        BucketBackup: backupBucket,
+    }, nil
 }
