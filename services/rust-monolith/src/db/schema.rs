d5b0062940a54784719218dde0ab3a67719986f1
ZiedYousfi <yousfi_zied@yahoo.com>
2025-07-03 22:08:01 +0200

    fix: update users table definition to use uuid as primary key in schema.rs

--- services/rust-monolith/src/db/schema.rs
+++ services/rust-monolith/src/db/schema.rs
@@ -1,8 +1,7 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
-    users (id) {
-        id -> Int4,
+    users (uuid) {
         uuid -> Text,
         clerk_user_id -> Text,
         created_at -> Timestamp,
